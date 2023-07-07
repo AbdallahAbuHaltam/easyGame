8af4b29abb7373d095019b2a44c21987e5d11976
AbdullahAbuHaltam <abuhaltamabdullah@gmail.com>
2023-07-08 02:37:52 +0300

    Finished Game

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -10,11 +11,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-     
-      home:Scaffold(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
         backgroundColor: Colors.indigo,
         appBar: AppBar(
-          title:const Text("تطابق صورة"),
+          title: const Text("تطابق صورة"),
           backgroundColor: Colors.indigo[800],
         ),
         body: const ImagePage(),
@@ -23,12 +24,60 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class ImagePage extends StatelessWidget {
+class ImagePage extends StatefulWidget {
   const ImagePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
+  State<ImagePage> createState() => _ImagePageState();
 }
 
+class _ImagePageState extends State<ImagePage> {
+  var leftimagenumber = 1;
+  var rightimagenumber = 2;
+  void changeImage() {
+    leftimagenumber = Random().nextInt(9) + 1;
+    rightimagenumber = Random().nextInt(9) + 1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Text(
+          leftimagenumber == rightimagenumber
+              ? 'مبروك لقد نجحت'
+              : 'حاول مرة اخرى',
+          style: const TextStyle(fontSize: 42.0, color: Colors.white),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Row(
+            children: [
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      changeImage();
+                    });
+                  },
+                  child: Image.asset('images/image-$leftimagenumber.png'),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      changeImage();
+                    });
+                  },
+                  child: Image.asset('images/image-$rightimagenumber.png'),
+                ),
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
